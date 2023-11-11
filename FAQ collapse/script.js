e77ab8b12268dfd1c7a3756152291a2abde07286
md_shoeb <mdshoebofficial2050@gmail.com>
2023-11-11 15:43:38 +0530

    notes.md added

--- FAQ collapse/script.js
+++ FAQ collapse/script.js
@@ -1,5 +1,6 @@
 const toggles = document.querySelectorAll(".faq-toggle");
 
+
 toggles.forEach((toggle) => {
   toggle.addEventListener("click", () => {
     toggle.parentNode.classList.toggle("active");
